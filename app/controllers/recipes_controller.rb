7ea6be8292c67b7b7cd5bdc5dc7390e0e6e9abf3
Justin Ndwiga <ndwigajustin@gmail.com>
2017-08-13 12:25:20 +0000

    Recipe model and controller, create action

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,28 @@
+class RecipesController < ApplicationController
+    before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @recipe = Recipe.all.order("created_at DESC")
+    end
+    def new
+        @recipe = Recipe.new
+    end
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+            redirect_to @recipe, notice: "Successfuly created new recipe"
+        else
+            render 'new'
+        end
+    end
+    def show
+    end
+    
+    private
+  def recipe_params
+     params.require(:recipe).permit(:title, :description)
+  end
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
