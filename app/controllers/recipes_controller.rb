545c1d6c6d63d5159d6498c062a92615fc5cb1ee
Justin Ndwiga <ndwigajustin@gmail.com>
2017-08-13 12:37:43 +0000

    Edit and Destroy Actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,20 @@ class RecipesController < ApplicationController
     end
     def show
     end
+    def edit
+    end
+    def update
+        if @recipe.update(recipe_params)
+            redirect_to @recipe, notice: "Successfuly updated recipe"
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @recipe.destroy
+        redirect_to root_path, notice: "Successfuly deleted recipe"
+    end
+
     
     private
   def recipe_params
